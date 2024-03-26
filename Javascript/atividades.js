c910b3cb30cf1a8d63cf208633b33ab8e64ce0c3
IC12 <iaracamargos@hotmail.com>
2024-03-26 12:26:21 -0300

    atividades

---
+++ Javascript/atividades.js
@@ -0,0 +1,75 @@
+//For of trabalha com valores
+function attNumbersForOf() {
+    const numbers = [2, 4, 8, 10, 32, 64, 128, 250];
+    
+    let sum = 0;
+    
+    for (let num of numbers) {
+        sum += num;
+    }
+    
+    console.log('Soma: ' + sum);
+    console.log('Média: ' + sum / numbers.length);
+}
+
+//For in trabalha com índices
+function attNotasBimestre() {
+    const notas = {
+        bimestre1: 7.8,
+        bimestre2: 3.7,
+        bimestre3: 6.4,
+        bimestre4: 8.8
+    };
+
+    let soma = 0;
+    let contagem = 0;
+
+    for (let bimestre in notas) {
+        contagem += 1;
+        soma += notas[bimestre];
+    }
+
+    console.log('Soma total das notas: ' + soma);
+
+    let media = soma / contagem;
+
+    if (media >= 7)
+        console.log('O aluno foi aprovado! ' + media);
+    else
+        console.log('O aluno foi reprovado ' + media);
+}
+
+function attYearsDoWhile() {
+    let birthYear = 2000;
+    let currentYear = 2024;
+    
+    do {
+        console.log(birthYear);
+    
+        birthYear++;
+    } while (birthYear <= currentYear);
+}
+
+function attBreakContinue() {
+    const customers = [
+        { name: "Ana", age: "37", credit: 4500.0 },
+        { name: "Robert", age: "67", credit: 10000.0 },
+        { name: "John", age: "15", credit: 1500.0 },
+        { name: "Maria", age: "20", credit: 2000.0 },
+    ];
+    
+    for (person of customers) {
+        //if (person.credit < 2000) continue;
+        if (person.credit < 2000) break;
+    
+        console.log("=======================");
+        console.log("Name: ", person.name);
+        console.log("Age: ", person.age);
+        console.log("Credit: ", person.credit);
+    }
+}
+
+attNumbersForOf();
+attNotasBimestre();
+attYearsDoWhile();
+attBreakContinue();
